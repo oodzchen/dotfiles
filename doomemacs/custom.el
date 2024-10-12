7cd25a0f35b2cb9fee0e3e2e09a3b5ffcea6b6b7
Kholin <oodzchen@gmail.com>
2024-10-12 19:37:05 +0800

    Add doom emacs configs

---
+++ doomemacs/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("10fef6d73ae453f39c9f325915386d41894870b72926e8e9b0c39d030447b703" default))
+ '(package-selected-packages '(zeno-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
